b302a0b552bd700b858a7a289f04acaa31fcbf89
Kirill Owen <kirill.owen@bigchange.com>
2024-01-11 10:22:55 +0000

    init

---
+++ AWSSDK.Log4Net.Repro/Program.cs
@@ -0,0 +1,59 @@
+﻿using BigChange.Automation.Service;
+using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using Topshelf;
+
+namespace AWSSDK.Log4Net.Repro
+{
+    class Program
+    {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        static void Main(string[] args)
+        {
+            XmlConfigurator.ConfigureAndWatch(new FileInfo("Log4Net.Config"));
+            try
+            {
+                Log.Info("Starting...");
+                HostFactory.Run(x =>
+                {
+                    x.SetDescription("AWSSDK.Log4Net.Repro.Service");
+                    x.SetDisplayName("AWSSDK.Log4Net.Repro.Service");
+                    x.SetServiceName("AWSSDK.Log4Net.Repro.Service");
+                    x.StartAutomatically();
+
+                    x.RunAsLocalSystem();
+                    x.UseLog4Net();
+                    
+                    x.EnableServiceRecovery(r =>
+                    {
+                        r.RestartService(1);
+                        r.SetResetPeriod(1); 
+                    });
+
+                    x.OnException(ex =>
+                    {
+                        File.AppendAllText(@"C:\Temp\Service.txt", ex.ToString());
+                    });
+
+                    x.Service<TestService>(s =>
+                    {
+                        s.ConstructUsing(name =>
+                        {
+                              return new TestService();
+                        });
+                        s.WhenStarted(acs => acs.Start());
+                        s.WhenStopped(acs => acs.Stop());
+                        s.WhenShutdown(acs => acs.Stop());
+                    });
+                });
+            }
+            catch (Exception e)
+            {
+                Log.ErrorFormat("A Fatal error has occured while initilaising the service it will restart \r\n\n{0}", e);
+            }
+        }
+    }
+}
---
+++ AWSSDK.Log4Net.Repro/TestService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using log4net;
+
+namespace BigChange.Automation.Service
+{
+    public class TestService
+    {
+        private static readonly ILog Log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+        private CancellationTokenSource _cancel;
+
+        public TestService()
+        {
+            _cancel = new CancellationTokenSource();
+
+            Task.Run(() => Background(_cancel.Token));
+        }
+
+        public bool Start()
+        {
+            return true;
+        }
+
+        public bool Stop()
+        {
+            return true;
+        }
+        async Task Background(CancellationToken cancellationToken)
+        {
+            while (!cancellationToken.IsCancellationRequested)
+            {
+                await Task.Delay(1000, cancellationToken);
+                
+                Log.Info($"Service running at: {DateTime.UtcNow}");
+            }
+        }
+    }
+}
